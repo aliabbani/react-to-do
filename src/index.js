6adb6cf0f1795bdc7bd9b647b16cfab77d420ffe
ali abbani <aliox@ahlam.com>
2022-03-01 13:43:48 +0200

    This branch is not done yet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import TodoContainer from "./functionBased/components/TodoContainer"
+import TodoContainer from "./classBased/components/TodoContainer"
 import './functionBased/App.css'
 
 ReactDOM.render(
